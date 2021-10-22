2a184c6aa48b1ea23b239522783276eeed878223
aseemsavio <aseemsavio3@gmail.com>
2021-10-22 20:56:02 +0530

    service apis defined

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 Open API for the Latin Vulgate, its English Translation - Catholic Public Domain Version [CPDV] and study notes.
 
 This project is currently under development. Watch the release space for updates on the first release.
+
+### Building
+
+To launch your tests:
+```
+./gradlew clean test
+```
+
+To package your application:
+```
+./gradlew clean assemble
+```
+
+To run your application:
+```
+./gradlew clean run
+```
--- src/main/kotlin/com/aseemsavio/biblia/data/service/api/BibiliaService.kt
+++ src/main/kotlin/com/aseemsavio/biblia/data/service/api/BibiliaService.kt
@@ -1,20 +1,38 @@
 package com.aseemsavio.biblia.data.service.api
 
-import com.aseemsavio.biblia.data.preparation.BookNamesItem
-import com.aseemsavio.biblia.data.preparation.JsonBook
-import com.aseemsavio.biblia.data.preparation.JsonTestament
+import com.aseemsavio.biblia.data.BibleBookName
+import com.aseemsavio.biblia.data.BibleChapter
+import com.aseemsavio.biblia.data.Testament
+import com.aseemsavio.biblia.data.database.api.TotalChapters
+import com.aseemsavio.biblia.data.database.api.TotalVerses
+import com.aseemsavio.biblia.data.database.api.VerseNumber
+import com.aseemsavio.biblia.data.preparation.*
 
-interface BibiliaService : TestamentService, BookService
+interface BibiliaService : TestamentService, BookService, ChaptersService, VersesService
 
 interface TestamentService {
   fun getTestamentNames(): Set<String>
-  fun getTestaments(testamentName: String?): JsonTestamentList
+  fun getTestaments(testamentName: String?): List<JsonTestament>
 }
 
 interface BookService {
-  fun getBookNames(testamentName: String?): BookNamesList
+  fun getBookNames(testamentName: String?): List<BookNamesItem>
   fun getBook(bookName: String): JsonBook?
 }
 
-typealias JsonTestamentList = List<JsonTestament>
-typealias BookNamesList = List<BookNamesItem>
+interface ChaptersService {
+  fun getTotalChapters(testament: Testament, book: BibleBookName): TotalChapters
+  fun getChapter(testament: Testament, book: BibleBookName, chapter: BibleChapter): List<JsonVerse>
+}
+
+interface VersesService {
+  fun getTotalVerses(testament: Testament, book: BibleBookName, chapter: BibleChapter): TotalVerses
+  fun getVerse(testament: Testament, book: BibleBookName, chapter: BibleChapter, verse: VerseNumber): JsonVerse?
+  fun getVerses(
+    testament: Testament,
+    book: BibleBookName,
+    chapter: BibleChapter,
+    from: VerseNumber,
+    to: VerseNumber
+  ): List<JsonVerse>?
+}
